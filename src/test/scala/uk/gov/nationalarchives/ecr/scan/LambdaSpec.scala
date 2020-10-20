99221b8005ad9bfb1cce419b5160dbaf534b18a3
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-20 11:17:37 +0100

    Add test json into files.

--- src/test/scala/uk/gov/nationalarchives/ecr/scan/LambdaSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/ecr/scan/LambdaSpec.scala
@@ -1,112 +1,120 @@
 package uk.gov.nationalarchives.ecr.scan
 
 import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock.{equalTo, okJson, post, urlEqualTo}
-import com.github.tomakehurst.wiremock.matching.StringValuePattern
+import com.github.tomakehurst.wiremock.client.WireMock._
 import org.mockito.{ArgumentCaptor, MockitoSugar}
+import org.mockito.ArgumentMatchers.any
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class LambdaSpec extends AnyFlatSpec with Matchers with MockitoSugar {
+import scala.io.Source.fromResource
+
+class LambdaSpec extends AnyFlatSpec with Matchers with MockitoSugar with BeforeAndAfterEach {
+
+  override def beforeEach(): Unit = {
+    server.resetAll()
+  }
 
   val server = new WireMockServer(9001)
-  server.stubFor(
-    post(urlEqualTo("/"))
-      .withHeader("X-Amz-Target", equalTo("AmazonEC2ContainerRegistry_V20150921.DescribeRepositories"))
-      .willReturn(okJson("""{
-                           |   "nextToken": "string",
-                           |   "repositories": [
-                           |      {
-                           |         "createdAt": 1,
-                           |         "encryptionConfiguration": {
-                           |            "encryptionType": "string",
-                           |            "kmsKey": "string"
-                           |         },
-                           |         "imageScanningConfiguration": {
-                           |            "scanOnPush": true
-                           |         },
-                           |         "imageTagMutability": "string",
-                           |         "registryId": "string",
-                           |         "repositoryArn": "string",
-                           |         "repositoryName": "string",
-                           |         "repositoryUri": "string"
-                           |      },
-                           |      {
-                           |         "createdAt": 1,
-                           |         "encryptionConfiguration": {
-                           |            "encryptionType": "string",
-                           |            "kmsKey": "string"
-                           |         },
-                           |         "imageScanningConfiguration": {
-                           |            "scanOnPush": true
-                           |         },
-                           |         "imageTagMutability": "string",
-                           |         "registryId": "string",
-                           |         "repositoryArn": "string",
-                           |         "repositoryName": "string",
-                           |         "repositoryUri": "string"
-                           |      }
-                           |   ]
-                           |}""".stripMargin))
-  )
-  server.stubFor(
-    post(urlEqualTo("/"))
-      .withHeader("x-amz-target", equalTo("AmazonEC2ContainerRegistry_V20150921.DescribeImages"))
-      .willReturn(okJson("""{
-                           |   "imageDetails": [
-                           |      {
-                           |         "artifactMediaType": "string",
-                           |         "imageDigest": "string",
-                           |         "imageManifestMediaType": "string",
-                           |         "imagePushedAt": 1,
-                           |         "imageScanFindingsSummary": {
-                           |            "findingSeverityCounts": {
-                           |               "string" : 1
-                           |            },
-                           |            "imageScanCompletedAt": 1,
-                           |            "vulnerabilitySourceUpdatedAt": 1
-                           |         },
-                           |         "imageScanStatus": {
-                           |            "description": "string",
-                           |            "status": "string"
-                           |         },
-                           |         "imageSizeInBytes": 1,
-                           |         "imageTags": [ "string" ],
-                           |         "registryId": "string",
-                           |         "repositoryName": "string"
-                           |      }
-                           |   ],
-                           |   "nextToken": "string"
-                           |}""".stripMargin))
-  )
+  server.start()
 
-  server.stubFor(
-    post(urlEqualTo("/"))
-      .withHeader("x-amz-target", equalTo("AmazonEC2ContainerRegistry_V20150921.StartImageScan"))
-      .willReturn(okJson("""{
-                           |   "imageId": {
-                           |      "imageDigest": "string",
-                           |      "imageTag": "string"
-                           |   },
-                           |   "imageScanStatus": {
-                           |      "description": "string",
-                           |      "status": "string"
-                           |   },
-                           |   "registryId": "string",
-                           |   "repositoryName": "string"
-                           |}""".stripMargin))
-  )
+  def stubDescribeRepositoriesResponse(fileName: String) = {
+    server.stubFor(
+      post(urlEqualTo("/"))
+        .withHeader("X-Amz-Target", equalTo("AmazonEC2ContainerRegistry_V20150921.DescribeRepositories"))
+        .willReturn(okJson(fromResource(s"json/$fileName.json").mkString))
+    )
+  }
 
-  "it" should "test things" in {
-    server.start()
+  def stubDescribeImageResponse(repositoryName: String) = {
+    val body: String = s"""{"repositoryName":"$repositoryName"}"""
+    server.stubFor(
+      post(urlEqualTo("/"))
+        .withHeader("X-Amz-Target", equalTo("AmazonEC2ContainerRegistry_V20150921.DescribeImages"))
+        .withRequestBody(binaryEqualTo(body.getBytes(Charset.defaultCharset())))
+        .willReturn(okJson(fromResource(s"json/describe_images_response.json").mkString.replace("$repositoryName", repositoryName)))
+    )
+  }
+
+  def stubStartImageScanResponse(repositoryName: String) = {
+    val body = s"""{"repositoryName":"$repositoryName","imageId":{"imageDigest":"$repositoryName-digest","imageTag":"$repositoryName-latest"}}"""
+    server.stubFor(
+      post(urlEqualTo("/"))
+        .withHeader("X-Amz-Target", equalTo("AmazonEC2ContainerRegistry_V20150921.StartImageScan"))
+        .withRequestBody(binaryEqualTo(body.getBytes(Charset.defaultCharset())))
+        .willReturn(okJson(fromResource(s"json/start_image_scan_response.json").mkString.replace("$repositoryName", repositoryName)))
+    )
+  }
+
+  def stubFailedImageScanResponse(repositoryName: String) = {
+    val body = s"""{"repositoryName":"$repositoryName","imageId":{"imageDigest":"$repositoryName-digest","imageTag":"$repositoryName-latest"}}"""
+    server.stubFor(
+      post(urlEqualTo("/"))
+        .withHeader("X-Amz-Target", equalTo("AmazonEC2ContainerRegistry_V20150921.StartImageScan"))
+        .withRequestBody(binaryEqualTo(body.getBytes(Charset.defaultCharset())))
+        .willReturn(badRequest())
+    )
+  }
+
+  "the process method" should "return the correct status for a single repository" in {
+    val repositoryName = "repository1of1"
+    stubDescribeRepositoriesResponse("describe_repositories_one_repo")
+    stubDescribeImageResponse(repositoryName)
+    stubStartImageScanResponse(repositoryName)
     val stream = mock[ByteArrayOutputStream]
     val captor: ArgumentCaptor[Array[Byte]] = ArgumentCaptor.forClass(classOf[Array[Byte]])
     doNothing.when(stream).write(captor.capture())
     new Lambda().process(null, stream)
-    val a: String = captor.getAllValues.get(0).map(_.toChar).mkString
-    a
+    val output: String = captor.getAllValues.get(0).map(_.toChar).mkString
+    output should equal(s"$repositoryName-status")
   }
 
+  "the process method" should "return the correct status for a multiple repositories" in {
+    val repositoryNames = List("repository1of4","repository2of4","repository3of4","repository4of4")
+    stubDescribeRepositoriesResponse("describe_repositories_four_repos")
+    repositoryNames.foreach(repositoryName => {
+      stubDescribeImageResponse(repositoryName)
+      stubStartImageScanResponse(repositoryName)
+    })
+    val stream = mock[ByteArrayOutputStream]
+    val captor: ArgumentCaptor[Array[Byte]] = ArgumentCaptor.forClass(classOf[Array[Byte]])
+    doNothing.when(stream).write(captor.capture())
+    new Lambda().process(null, stream)
+    val output: String = captor.getAllValues.get(0).map(_.toChar).mkString
+    output should equal(repositoryNames.map(name => s"$name-status").mkString("\n"))
+  }
+
+  "the process method" should "return the correct status when one image scan fails" in {
+    val repositoryNames = List("repository1of4","repository2of4","repository3of4","repository4of4")
+    stubDescribeRepositoriesResponse("describe_repositories_four_repos")
+    repositoryNames.foreach(stubDescribeImageResponse)
+    repositoryNames.tail.foreach(stubStartImageScanResponse)
+    stubFailedImageScanResponse(repositoryNames.head)
+    val stream = mock[ByteArrayOutputStream]
+    val captor: ArgumentCaptor[Array[Byte]] = ArgumentCaptor.forClass(classOf[Array[Byte]])
+    doNothing.when(stream).write(captor.capture())
+    intercept[RuntimeException] {
+      new Lambda().process(null, stream)
+    }
+    val output: String = captor.getAllValues.get(0).map(_.toChar).mkString
+    output should equal(repositoryNames.tail.map(name => s"$name-status").mkString("\n"))
+  }
+
+  "the process method" should "throw an exception when there are errors" in {
+    val repositoryNames = List("repository1of4","repository2of4","repository3of4","repository4of4")
+    stubDescribeRepositoriesResponse("describe_repositories_four_repos")
+    repositoryNames.foreach(stubDescribeImageResponse)
+    repositoryNames.tail.foreach(stubStartImageScanResponse)
+    stubFailedImageScanResponse(repositoryNames.head)
+    val stream = mock[ByteArrayOutputStream]
+    doNothing.when(stream).write(any[Array[Byte]])
+    val error = intercept[RuntimeException] {
+      new Lambda().process(null, stream)
+    }
+    error.getMessage should be("null (Service: Ecr, Status Code: 400, Request ID: null, Extended Request ID: null)")
+  }
 }
