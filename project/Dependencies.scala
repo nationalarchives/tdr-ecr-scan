3785b14ab5bfa29d6bdf71f24c35d4408b118b6e
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2021-03-10 12:35:57 +0000

    Upgrade AWS utils library
    The signature of the ECR utils class has changed so that it doesn't make assumptions
    about how the client app stores the config, so pass in the new endpoint parameter.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val awsUtils =  "uk.gov.nationalarchives.aws.utils" %% "tdr-aws-utils" % "0.1.5"
+  lazy val awsUtils =  "uk.gov.nationalarchives.aws.utils" %% "tdr-aws-utils" % "0.1.11"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.2.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
