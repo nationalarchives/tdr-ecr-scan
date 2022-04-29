02c6a42a5b684340b1d084d2ba4f0dcac7afc62d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-04-29 08:25:08 +0000

    Update log4cats-core, log4cats-slf4j to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.5"
-  lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.2.0"
-  lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j"   % "2.2.0"
+  lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.3.0"
+  lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j"   % "2.3.0"
   lazy val slf4j = "org.slf4j" % "slf4j-simple" % "1.7.36"
 }
