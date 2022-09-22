ae93537aeadbbbda9001e8a0d9d0b8db06e55e21
Sam Palmer <sam.palmer@nationalarchives.gov.uk>
2022-09-22 10:49:36 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.36"
+  lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.38"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.14"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
