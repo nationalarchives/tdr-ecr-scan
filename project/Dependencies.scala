374847241aef39a40d15be4120c949431c227b4e
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-01-07 08:30:54 +0000

    Update scalatest to 3.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val ecrUtils =  "uk.gov.nationalarchives" %% "ecr-utils" % "0.1.62"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.4"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.12"
   lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.5.0"
