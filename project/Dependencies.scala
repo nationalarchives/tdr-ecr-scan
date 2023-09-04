3217053a23c690f9b66e131bdf10ba7456826780
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-04 08:30:14 +0000

    Update wiremock to 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val ecrUtils =  "uk.gov.nationalarchives" %% "ecr-utils" % "0.1.100"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.1"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
-  lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.0"
+  lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.22"
   lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.6.0"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.2"
