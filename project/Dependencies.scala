92c1e7a8dce553581a13780c9b4a8b02306317ba
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-28 08:29:48 +0000

    Update cats-effect to 3.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val ecrUtils =  "uk.gov.nationalarchives" %% "ecr-utils" % "0.1.104"
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.1"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.27"
