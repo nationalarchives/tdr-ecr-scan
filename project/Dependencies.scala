add7237ad821f9a8e14292330b9c366663a6f436
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-10-31 08:30:53 +0000

    Update mockito-scala to 1.17.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.27"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.29"
   lazy val log4cats = "org.typelevel" %% "log4cats-core"    % "2.6.0"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.3"
   lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j"   % "2.6.0"
